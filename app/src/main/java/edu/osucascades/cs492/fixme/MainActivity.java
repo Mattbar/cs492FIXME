e415c21c677f5c575b597eed95d3d7d82cdc1641
Mattbar <mlbarnes04@gmail.com>
2019-10-02 09:53:47 -0700

    fixed fixme01

---
+++ app/src/main/java/edu/osucascades/cs492/fixme/MainActivity.java
@@ -0,0 +1,57 @@
+package edu.osucascades.cs492.fixme;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button mFalseButton;
+    private Button mTrueButton;
+
+    private Question[] mQuestionBank = new Question[] {
+            new Question(R.string.question_text, true)
+    };
+    private int mCurrentIndex = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mTrueButton = findViewById(R.id.true_button);
+
+        mTrueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkAnswer(true);
+            }
+        });
+
+        mFalseButton = findViewById(R.id.false_button);
+        mFalseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                checkAnswer(false);
+            }
+        });
+    }
+
+    private void checkAnswer(boolean userPressedTrue) {
+        boolean answerIsTrue = mQuestionBank[mCurrentIndex].isAnswerTrue();
+
+        int messageResId = 0;
+
+        if(userPressedTrue == answerIsTrue) {
+            messageResId = R.string.valid_toast;
+        } else {
+            messageResId = R.string.incorrect_toast;
+        }
+
+        Toast answer_Toast = Toast.makeText(this, messageResId, Toast.LENGTH_SHORT);
+        answer_Toast.setGravity(Gravity.TOP, 0,0);
+        answer_Toast.show();
+    }
+}
